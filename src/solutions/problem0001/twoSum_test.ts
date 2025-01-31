c2768bc0d5b601edb7abb343ec86fa5362549d28
shilin83 <shilin_83@qq.com>
2025-01-31 21:21:55 +0800

    1.两数之和

---
+++ src/solutions/problem0001/twoSum_test.ts
@@ -0,0 +1,37 @@
+import { describe, expect, test } from 'bun:test'
+import twoSum from './twoSum'
+
+interface Case {
+	nums: number[]
+	target: number
+	expected: number[]
+}
+
+const cases: Case[] = [
+	{
+		nums: [2, 7, 11, 15],
+		target: 9,
+		expected: [0, 1],
+	},
+	{
+		nums: [3, 2, 4],
+		target: 6,
+		expected: [1, 2],
+	},
+	{
+		nums: [3, 3],
+		target: 6,
+		expected: [0, 1],
+	},
+	{
+		nums: [1, 2],
+		target: 4,
+		expected: [],
+	},
+]
+
+describe('1.两数之和', (): void => {
+	test.each(cases)('示例%#', ({ nums, target, expected }): void => {
+		expect(twoSum(nums, target)).toEqual(expected)
+	})
+})
